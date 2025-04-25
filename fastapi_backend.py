ba127e326794b8a15c768ce9cdc77fd190a0567f
konstan10 <kostya@bu.edu>
2025-04-25 16:10:59 -0400

    yay this works

---
+++ fastapi_backend.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.post("/data")
+async def receive_data(request: Request):
+    data = await request.json()
+    print(f"Humidity: {data['humidity']}%")
+    print(f"Temperature: {data['temperature']}°C")
+    return {"status": "ok"}
