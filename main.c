6775ce4ec908caa04e240211fb97311d007b46aa
konstan10 <kostya@bu.edu>
2025-04-26 12:38:30 -0400

    trying to reduce flickering

--- main.c
+++ main.c
@@ -4,29 +4,6 @@
 #include <curl/curl.h>
 #include <ncurses.h>
 
-void draw_border() {
-    int max_y, max_x;
-    getmaxyx(stdscr, max_y, max_x);
-    mvhline(0, 0, 0, max_x);
-    mvhline(max_y-1, 0, 0, max_x);
-    mvvline(0, 0, 0, max_y);
-    mvvline(0, max_x-1, 0, max_y);
-    mvaddch(0, 0, ACS_ULCORNER);
-    mvaddch(0, max_x-1, ACS_URCORNER);
-    mvaddch(max_y-1, 0, ACS_LLCORNER);
-    mvaddch(max_y-1, max_x-1, ACS_LRCORNER);
-}
-
-void setup_colors() {
-    start_color();
-    use_default_colors();
-    init_pair(1, COLOR_CYAN, -1);
-    init_pair(2, COLOR_GREEN, -1);
-    init_pair(3, COLOR_RED, -1);
-    init_pair(4, COLOR_YELLOW, -1);
-    init_pair(5, COLOR_WHITE, -1);
-}
-
 int main() {
     int type = DHT11;
     int gpio_base = 2;
@@ -41,46 +18,45 @@ int main() {
     keypad(stdscr, TRUE);
     timeout(1000);
 
-    setup_colors();
+    if (has_colors()) {
+        start_color();
+        init_pair(1, COLOR_CYAN, COLOR_BLACK);
+        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(3, COLOR_GREEN, COLOR_BLACK);
+        init_pair(4, COLOR_RED, COLOR_BLACK);
+    }
+
     curl_global_init(CURL_GLOBAL_ALL);
+
+    erase();
+    box(stdscr, 0, 0);
+    attron(COLOR_PAIR(1) | A_BOLD);
+    mvprintw(1, 4, "BeagleBone DHT11 Sensor Monitor");
+    attroff(COLOR_PAIR(1) | A_BOLD);
+
+    mvprintw(3, 4, "Result:");
+    mvprintw(4, 4, "Humidity:");
+    mvprintw(5, 4, "Temperature:");
+    mvprintw(7, 4, "[Refreshing every second]");
+    refresh();
 
     while (1) {
         int result = bbb_dht_read(type, gpio_base, gpio_number, &humidity, &temperature);
 
-        clear();
-        draw_border();
-
-        attron(COLOR_PAIR(1) | A_BOLD);
-        mvprintw(1, 4, "BeagleBone DHT11 Sensor Monitor");
-        attroff(COLOR_PAIR(1) | A_BOLD);
-
-        attron(COLOR_PAIR(5));
-        mvprintw(3, 6, "Result Code: ");
-        attron(COLOR_PAIR(2));
-        printw("%d", result);
-        attroff(COLOR_PAIR(2));
-
-        attron(COLOR_PAIR(5));
-        mvprintw(5, 6, "Humidity: ");
         attron(COLOR_PAIR(4));
-        printw("%.2f %%", humidity);
+        mvprintw(3, 13, "%3d   ", result);
         attroff(COLOR_PAIR(4));
 
-        attron(COLOR_PAIR(5));
-        mvprintw(7, 6, "Temperature: ");
-        if (temperature > 30.0) {
-            attron(COLOR_PAIR(3) | A_BOLD);
-        } else {
-            attron(COLOR_PAIR(2));
-        }
-        printw("%.2f deg C", temperature);
-        attroff(COLOR_PAIR(2) | COLOR_PAIR(3) | A_BOLD);
+        attron(COLOR_PAIR(2));
+        mvprintw(4, 14, "%6.2f %% ", humidity);
+        attroff(COLOR_PAIR(2));
 
-        attron(COLOR_PAIR(1));
-        mvprintw(10, 4, "[Refreshing every second]");
-        attroff(COLOR_PAIR(1));
+        attron(COLOR_PAIR(3));
+        mvprintw(5, 17, "%6.2f deg C", temperature);
+        attroff(COLOR_PAIR(3));
 
-        refresh();
+        wnoutrefresh();
+        doupdate();
 
         CURL *curl = curl_easy_init();
         if (curl) {
